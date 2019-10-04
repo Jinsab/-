87550ff37dd8c8c11ba37057d84bb68f939f5895
Simon <ysmxyz@naver.com>
2019-10-05 04:57:13 +0900

    Swipe

---
+++ Assets/01.Scripts/CTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CTest : MonoBehaviour
+{
+    public UnityEngine.UI.Text _tesxt;
+
+    private void Update()
+    {
+        if (SwipeInput.swipedDown)
+        {
+            _tesxt.text = "swipedDown";
+        }
+        if (SwipeInput.swipedUp)
+        {
+            _tesxt.text = "swipedUp";
+        }
+        if (SwipeInput.swipedRight)
+        {
+            _tesxt.text = "swipedRight";
+        }
+        if (SwipeInput.swipedLeft)
+        {
+            _tesxt.text = "swipedLeft";
+        }
+    }
+}
