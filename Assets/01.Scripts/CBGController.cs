33e5a943f43eadb897008c3e57a0079bec11fd75
Simon <ysmxyz@naver.com>
2019-10-05 23:24:27 +0900

    BG Scroll

---
+++ Assets/01.Scripts/CBGController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CBGController : MonoBehaviour
+{
+    public static CBGController _pInstance = null;
+
+    [SerializeField] private GameObject _prefCloud = null;
+    [SerializeField] private Sprite[] _imageClouds = null;
+    [SerializeField] private float _fCloudRegenTerm = 1f;
+    [SerializeField] private Transform _trCloudParent = null;
+
+    [SerializeField] private GameObject _prefWall = null;
+    [SerializeField] private Sprite[] _imageWalls = null;
+    [SerializeField] private Transform _trWallParent = null;
+
+    [SerializeField] private MeshRenderer _matGroundRenderer = null;
+
+    public float _fBGScrollSpeed = 1f;
+    public Transform _trLastWall = null;
+    public bool _bIsDestroyedWall = false;
+
+    private float _fCloudRegenTimeStack = 0f;
+
+    private void Awake()
+    {
+        if (_pInstance == null)
+        {
+            _pInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (CGameManager._pInstance._bIsScrollBG)
+        {
+            // Cloud Regen
+            _fCloudRegenTimeStack += Time.deltaTime;
+            if (_fCloudRegenTimeStack > _fCloudRegenTerm)
+            {
+                CloudRegen();
+                _fCloudRegenTimeStack -= _fCloudRegenTerm;
+            }
+
+            // Ground Move
+            float offsetX = _matGroundRenderer.material.mainTextureOffset.x + (Time.deltaTime * _fBGScrollSpeed);
+            if (offsetX > 1f) offsetX--;
+
+            _matGroundRenderer.material.mainTextureOffset = new Vector2(offsetX, 0f);
+        }
+    }
+
+    public void WallRegen()
+    {
+        Vector3 pos = new Vector3(_trLastWall.position.x + 1.6f,
+            _trLastWall.position.y, _trLastWall.position.z);
+
+        CWall obj = Instantiate(_prefWall, pos, Quaternion.identity).GetComponent<CWall>();
+        obj.transform.SetParent(_trWallParent);
+        _trLastWall = obj.transform;
+
+        int randidx = Random.Range(0, _imageWalls.Length);
+        obj.DoInit(_imageWalls[randidx]);
+        _bIsDestroyedWall = false;
+    }
+
+    private void CloudRegen()
+    {
+        float posY = Random.Range(4.5f, 6.3f);
+        CCloud obj = Instantiate(_prefCloud, new Vector3(6f, posY, 0f), Quaternion.identity).GetComponent<CCloud>();
+        obj.transform.SetParent(_trCloudParent);
+
+        int randidx = Random.Range(0, _imageClouds.Length);
+        obj.DoInit(_imageClouds[randidx]);
+    }
+}
--- Assets/01.Scripts/CGameManager.cs
+++ Assets/01.Scripts/CGameManager.cs
@@ -4,12 +4,25 @@ using UnityEngine.UI;
 
 public class CGameManager : MonoBehaviour
 {
+    public enum GAME_STATE
+    {
+        NULL = -1,
+        QUIZ,
+        PRE_ACTION,
+        ACTION,
+        GAMEOVER,
+        NUMBEROFTYPES
+    }
+
     public static CGameManager _pInstance;
 
+    [SerializeField] private CCharacter _character = null;
     [SerializeField] private CameraShake _cameraShake = null;
     [SerializeField] private MeshRenderer _bgMsr = null;
     [SerializeField] private Image _uiFade = null;
+    [SerializeField] private Transform _trEnemyPrent = null;
 
+    public GAME_STATE _eState = GAME_STATE.NULL;
     public bool _bIsScrollBG = false;
     public float _fBGScrollSpeed = 1f;
 
@@ -25,21 +38,42 @@ public class CGameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        DoInit();
+    }
+
     private void Update()
     {
-        Debug.Log(_bgMsr.material.mainTextureOffset);
+        if (_eState == GAME_STATE.PRE_ACTION)
+        {
+
+        }
+
         if (_bIsScrollBG == true)
         {
-            float offsetX = _bgMsr.material.mainTextureOffset.x + (Time.deltaTime * _fBGScrollSpeed);
-            if (offsetX > 1f) offsetX--;
-
-            _bgMsr.material.mainTextureOffset = new Vector2(offsetX, 0f);
+            BGScroll();
         }
+    }
+
+    public void DoInit()
+    {
+        _eState = GAME_STATE.PRE_ACTION;
+        _bIsScrollBG = false;
+        _character.SetLanding(true);
     }
 
     public void PlayerTakeDamage()
     {
         _cameraShake.shakeDuration = 0.2f;
+    }
+
+    private void BGScroll()
+    {
+        float offsetX = _bgMsr.material.mainTextureOffset.x + (Time.deltaTime * _fBGScrollSpeed);
+        if (offsetX > 1f) offsetX--;
+
+        _bgMsr.material.mainTextureOffset = new Vector2(offsetX, 0f);
     }
 
     IEnumerator Fadeout()
