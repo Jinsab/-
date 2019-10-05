3e0a61bd0f3a37c0e52b515c23ac3c799dda955f
Simon <ysmxyz@naver.com>
2019-10-06 08:30:41 +0900

    Title

---
+++ Assets/01.Scripts/CTitle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CTitle : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Quiz");
+        }
+    }
+}
