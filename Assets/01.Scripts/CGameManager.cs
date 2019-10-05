ceefc5c2ab3139821a41a4892ae08f50cbe4f6d2
Simon <ysmxyz@naver.com>
2019-10-06 00:55:40 +0900

    Enemy Regen

--- Assets/01.Scripts/CGameManager.cs
+++ Assets/01.Scripts/CGameManager.cs
@@ -20,11 +20,13 @@ public class CGameManager : MonoBehaviour
     [SerializeField] private CameraShake _cameraShake = null;
     [SerializeField] private MeshRenderer _bgMsr = null;
     [SerializeField] private Image _uiFade = null;
+    [SerializeField] private GameObject[] _prefEnemys = null;
+    [SerializeField] private Transform[] _trEnemyRegenPos = null;
     [SerializeField] private Transform _trEnemyPrent = null;
 
     public GAME_STATE _eState = GAME_STATE.NULL;
     public bool _bIsScrollBG = false;
-    public float _fBGScrollSpeed = 1f;
+    public float _fCharacterActPos = 0f;
 
     private void Awake()
     {
@@ -47,33 +49,32 @@ public class CGameManager : MonoBehaviour
     {
         if (_eState == GAME_STATE.PRE_ACTION)
         {
+            _character.transform.Translate(Vector3.left * Time.deltaTime * 5f);
 
-        }
+            if (_character.transform.position.x < _fCharacterActPos)
+            {
+                _character.transform.position = new Vector3(_fCharacterActPos, 
+                    _character.transform.position.y, _character.transform.position.z);
 
-        if (_bIsScrollBG == true)
-        {
-            BGScroll();
+                _character.SetLanding(false);
+
+                _eState = GAME_STATE.ACTION;
+
+                StartCoroutine(RegenEnemy());
+            }
         }
     }
 
     public void DoInit()
     {
         _eState = GAME_STATE.PRE_ACTION;
-        _bIsScrollBG = false;
+        _bIsScrollBG = true;
         _character.SetLanding(true);
     }
 
     public void PlayerTakeDamage()
     {
         _cameraShake.shakeDuration = 0.2f;
-    }
-
-    private void BGScroll()
-    {
-        float offsetX = _bgMsr.material.mainTextureOffset.x + (Time.deltaTime * _fBGScrollSpeed);
-        if (offsetX > 1f) offsetX--;
-
-        _bgMsr.material.mainTextureOffset = new Vector2(offsetX, 0f);
     }
 
     IEnumerator Fadeout()
@@ -83,5 +84,23 @@ public class CGameManager : MonoBehaviour
             _uiFade.color = new Color(0f, 0f, 0f, _uiFade.color.a + Time.deltaTime);
             yield return null;
         }
+    }
+
+    IEnumerator RegenEnemy()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while(true)
+        {
+            int randEnemyIdx = Random.Range(0, _prefEnemys.Length);
+
+            CEnemy enemy = Instantiate(_prefEnemys[randEnemyIdx], 
+                _trEnemyRegenPos[randEnemyIdx].position, Quaternion.identity).GetComponent<CEnemy>();
+
+            enemy.DoInit();
+            enemy.transform.SetParent(_trEnemyPrent);
+
+            yield return new WaitForSeconds(2f);
+        }        
     }
 }
